b353ad82e99a7564c38c341abbf6f82db40fd8df
Titanium2099 <yasgh@protonmail.com>
2024-01-29 19:45:55 -0800

    create new tabs + some functionality

--- src/Tests/TopBar.js
+++ src/Tests/TopBar.js
@@ -16,29 +16,74 @@ class TopBar {
             }
         }
         this.add = function (name, icon_name, edited = false) {
-         if(this._data.filesKnown.indexOf(icon_name) != -1){
-            var element = document.createElement("div");
-            var icon = document.createElement("img");
-            console.log(icon_name,this._data.filesKnown.indexOf(icon_name))
-            icon.src = "/assets/" + this._data.fileIcons[icon_name];
-            icon.className = "icon";
-            element.appendChild(icon);
-            element.className = "Tab";
-            var p = document.createElement("p");
-            p.innerHTML = name;
-            element.appendChild(p);
-            var i = document.createElement("div");
-            i.className = 'actions';
-            if(edited){
-                i.innerHTML = this._data.editedSVG;
-            }else{
-                i.innerHTML = this._data.crossedSVG;
+            if (this._data.filesKnown.indexOf(icon_name) != -1) {
+                var element = document.createElement("div");
+                var icon = document.createElement("img");
+                console.log(icon_name, this._data.filesKnown.indexOf(icon_name))
+                icon.src = "/assets/" + this._data.fileIcons[icon_name];
+                icon.className = "icon";
+                element.appendChild(icon);
+                element.className = "Tab";
+                var p = document.createElement("p");
+                p.innerHTML = name;
+                element.appendChild(p);
+                var i = document.createElement("div");
+                i.className = 'actions';
+                if (edited) {
+                    i.innerHTML = this._data.editedSVG;
+                    element.setAttribute('edited', "true");
+                } else {
+                    i.innerHTML = this._data.crossedSVG;
+                }
+                function internalEvents(ParentThis) {
+                    const target = ParentThis._data.target;
+                    const editedSVG = ParentThis._data.editedSVG;
+                    const crossedSVG = ParentThis._data.crossedSVG;
+                    i.children[0].addEventListener("click", function () {
+                        element.remove();
+                        target.dispatchEvent(new CustomEvent('tabClosed', { detail: { id: element.id } }));
+                    })
+                    i.children[0].addEventListener('mouseover', function () {
+                        if (element.getAttribute('edited') == "true") {
+                            i.innerHTML = crossedSVG;
+                            internalEvents(ParentThis);
+                        }
+                    });
+                    i.children[0].addEventListener('mouseout', function () {
+                        if (element.getAttribute('edited') == "true") {
+                            i.innerHTML = editedSVG;
+                            internalEvents(ParentThis);
+                        }
+                    });
+                    i.children[0].addEventListener('mouseup', function () {
+                        element.remove();
+                        target.dispatchEvent(new CustomEvent('tabClosed', { detail: { id: element.id } }));
+                    });
+                }
+                internalEvents(this);
+                element.appendChild(i);
+                element.id = this._data.currID;
+                element.addEventListener('click', function () {
+                    this.setActive(element.id);
+                    this._data.target.dispatchEvent(new CustomEvent('ActivetabChanged', { detail: { id: element.id } }));
+                }.bind(this));
+                this._data.currID++;
+                this._data.target.appendChild(element);
+                //if active doesn't exist than set this tab as active
+                if (this._data.activeElement == null) {
+                    this.setActive(element.id);
+                }
+                return element.id;
+            } else {
+                throw new Error("File type is not known");
             }
-            element.appendChild(i);
-            this._data.target.appendChild(element);
-         }else{
-            throw new Error("File type is not known");
-         }
+        }
+        this.setActive = function (id) {
+            if (this._data.activeElement != null) {
+                this._data.target.children[this._data.activeElement].classList.remove("active");
+            }
+            this._data.target.children[id].classList.add("active");
+            this._data.activeElement = id;
         }
         //internal data
         this._data = {
@@ -74,6 +119,8 @@ class TopBar {
                 ogg: "audio.svg",
             },
             target: null,
+            activeElement: null,
+            currID: 0,
             editedSVG: '<div></div>',
             crossedSVG: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>',
         }
