78c179d9379c3db4a416d09a0c3b065d167bdbc7
Titanium2099 <yasgh@protonmail.com>
2024-02-03 13:41:09 -0800

    very basic Project FIle Structure Loader

---
+++ src/backend/loadProjectFileStructure.php
@@ -0,0 +1,65 @@
+<?php
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+header('Content-Type: application/json');
+
+$PROJECTID = $_GET['PID'];
+if (!isset($PROJECTID)) {
+    echo json_encode(array('status' => 'false', 'message' => 'No project ID provided'));
+    exit();
+}
+
+function listFilesRecursively($subfolder, $OGPath)
+{
+    $files = glob($subfolder . '/*');
+
+    $fileInfoArray = [];
+
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            $Finalfile = str_replace($OGPath . '/', '', $file);
+            if($Finalfile == ''){
+                $Finalfile = '/';
+            }
+            if($Finalfile[0] != '/'){
+                $Finalfile = '/' . $Finalfile;
+            }
+            //add the directory to the file list
+            $fileInfoArray[] = [
+                'PATH' => $Finalfile,
+                'FOLDER/FILE' => 'FOLDER',
+                'FILENAME' => basename($file)
+            ];
+            // If $file is a directory, recursively call the function
+            $fileInfoArray = array_merge($fileInfoArray, listFilesRecursively($file, $OGPath));
+        } else {
+            //remove the whole subfolder path from the file path
+            $file = str_replace($OGPath . '/', '', $file);
+            $fileName = basename($file);
+            $file = str_replace($fileName, '', $file);
+            if($file == ''){
+                $file = '/';
+            }
+            if($file[0] != '/'){
+                $file = '/' . $file;
+            }
+            $fileInfoArray[] = [
+                'PATH' => $file,
+                'FOLDER/FILE' => is_dir($file) ? 'FOLDER' : 'FILE',
+                'FILENAME' => $fileName
+            ];
+        }
+    }
+
+    return $fileInfoArray;
+}
+
+$currentPath = __DIR__;
+// Remove /backend from the end of the path
+$currentPath = substr($currentPath, 0, -8);
+$currentPath = $currentPath . '/projects/' . $PROJECTID;
+$fileList = listFilesRecursively($currentPath,$currentPath);
+//convert to JSON
+echo json_encode(array('status'=>'true','files' => $fileList));
+?>
